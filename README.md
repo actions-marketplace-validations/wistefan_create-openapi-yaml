dcbfb05016bf94b106adfc1808c8e0c45300ba40
Stefan Wiedemann <wistefan@googlemail.com>
2021-05-07 10:14:47 +0200

    intial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Create openapi.yaml github action
+
+This action creates an [OpenAPI](https://swagger.io/specification/)-representation of a given [Smart-Data-Model](https://smartdatamodels.org/). The rest-path is based on the [NGSI-LD Specification](https://www.etsi.org/deliver/etsi_gs/CIM/001_099/009/01.04.02_60/gs_cim009v010402p.pdf).
+
+The action will create the yaml-file inside github-workspace folder for further usage.
+
+## Inputs
+
+### `model-name`
+
+**Required** Name of the model to be generated. Should be the folder that holds the model.yaml.
+
+### `repository-name`
+
+**Required** Name of the repository containing the model.
+
+### `root-model-url`
+
+**Required** Url of the root model. Default `"https://smart-data-models.github.io"`.
+
+### `output-file-name`
+
+**Required** Name of the file to be generated. Default `"swagger.yaml"`.
+
+
+## Example usage
+
+uses: actions/create-openapi-yaml@v1
+with:
+  model-name: 'ACMeasurement'
+  repository-name: 'dataModel.Energy'
---
+++ build-yaml.py
@@ -0,0 +1,84 @@
+# Generates the swagger.yaml for the given data model
+
+import yaml
+import os
+import json
+
+
+def read_yaml(fileUrl):
+
+    file = open(fileUrl, "r")
+    return yaml.safe_load(file.read())
+
+
+modelYaml = os.environ['MODEL_FOLDER']+"/model.yaml"
+outputFile = os.environ['MODEL_FOLDER']+"/"+os.environ['OUTPUT_FILENAME']
+
+dataModel = os.environ['DATA_MODEL']
+repoName = os.environ['REPOSITORY_NAME']
+rootModelUrl = os.environ['ROOT_MODEL_URL']
+modelDict = read_yaml(modelYaml)
+print(type(modelDict))
+
+swaggerHeader = """---
+# Copyleft (c) 2021 Contributors to Smart Data Models initiative
+# """
+swaggerBody = f"""paths: 
+  /ngsi-ld/v1/entities: 
+    get: 
+      description: "Retrieve a set of entities which matches a specific query from an NGSI-LD system"
+      parameters: 
+        - in: query
+          name: type
+          required: true
+          schema: 
+            enum: 
+              - {dataModel}
+            type: string
+      responses: 
+        "200": 
+          content: 
+            application/ld+json: 
+              examples: 
+                keyvalues: 
+                  summary: "Key-Values Pairs"
+                  value: 
+                    - $ref: "{rootModelUrl}/{repoName}/{dataModel}/examples/example.json"
+                normalized: 
+                  summary: "Normalized NGSI-LD"
+                  value: 
+                    - $ref: "{rootModelUrl}/{repoName}/{dataModel}/examples/example-normalized.jsonld"
+          description: OK
+      tags: 
+        - ngsi-ld
+        - {dataModel}
+tags: 
+  - description: "NGSI-LD Linked-data Format"
+    name: ngsi-ld
+  - description: "{modelDict[dataModel]["description"]}"
+    name: {dataModel}"""
+
+tab = "  "
+nl = chr(10)
+swaggerContent = swaggerHeader + nl
+swaggerContent += nl
+swaggerContent += nl
+swaggerContent += "components:" + nl
+swaggerContent += 1 * tab + "schemas: " + nl
+swaggerContent += 2 * tab + dataModel + ": " + nl
+swaggerContent += 3 * tab + "$ref: " + chr(34) + rootModelUrl + repoName + "/" + dataModel + "/" + modelYaml + "#/" + dataModel + chr(34) + nl
+swaggerContent += "info:" + nl
+swaggerContent += 1 * tab + "description:  |" + nl
+if "description" in modelDict[dataModel]:
+    swaggerContent += 2 * tab + modelDict[dataModel]["description"] + nl
+swaggerContent += 1 * tab + "title: " + dataModel + nl
+swaggerContent += 1 * tab + "version: " + chr(34) + "1.0.0" + chr(34) + nl
+swaggerContent += "openapi: " + chr(34) + "3.0.0" + chr(34) + nl
+swaggerContent += nl
+
+for line in swaggerBody.splitlines():
+    print(line)
+    swaggerContent += line + nl
+print(swaggerContent)
+with open(outputFile, "w") as output:
+    output.write(swaggerContent)
